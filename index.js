4ad7a5f7b4b341eaf02408cd1fb5a67a5e16f260
chris <christsironiss@gmail.com>
2022-06-23 19:52:09 +0300

    no delay at all

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ io.on("connection", (socket) => {
 	socket.on("pause", ({ videoTime, user, room, dateEmited }) =>{
 		const dateNow = Date.now();
 		const emitionDelay = dateNow - dateEmited;
-		rooms[room].timestamp = (dateNow - rooms[room].date) - emitionDelay;
+		rooms[room].timestamp = (dateNow - rooms[room].date) ;
 		rooms[room].ispaused = true;
 		socket.to(room).emit("pause", {videoTime: rooms[room].timestamp, user: user, dateEmited: Date.now()});
 		console.log("user paused "," serverTimestamp= ", rooms[room].timestamp ," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
@@ -78,7 +78,7 @@ io.on("connection", (socket) => {
 	socket.on("play", ({ videoTime, user, room, dateEmited }) =>{
 		const dateNow = Date.now();
 		const emitionDelay = dateNow - dateEmited;
-		rooms[room].date = (dateNow - videoTime) + emitionDelay;		
+		rooms[room].date = (dateNow - videoTime) ;		
 		console.log("user played "," serverTimestamp= ", rooms[room].timestamp ," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
 
 		socket.to(room).emit("play", {videoTime: videoTime + emitionDelay, user: user, dateEmited: Date.now()});
