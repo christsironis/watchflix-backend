5015e1cb28f3c80b531f18d8d90b6d75e3dfd6b2
chris <christsironiss@gmail.com>
2022-06-23 19:40:00 +0300

    should i add or subtract delay from pause / play?

--- index.js
+++ index.js
@@ -73,13 +73,13 @@ io.on("connection", (socket) => {
 		rooms[room].timestamp = (dateNow - rooms[room].date) - emitionDelay;
 		rooms[room].ispaused = true;
 		socket.to(room).emit("pause", {videoTime: rooms[room].timestamp, user: user, dateEmited: Date.now()});
-		console.log("user paused "," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
+		console.log("user paused "," serverTimestamp= ", rooms[room].timestamp ," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
 	});
 	socket.on("play", ({ videoTime, user, room, dateEmited }) =>{
 		const dateNow = Date.now();
 		const emitionDelay = dateNow - dateEmited;
 		rooms[room].date = (dateNow - videoTime) + emitionDelay;		
-		console.log("user played "," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
+		console.log("user played "," serverTimestamp= ", rooms[room].timestamp ," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
 
 		socket.to(room).emit("play", {videoTime: videoTime + emitionDelay, user: user, dateEmited: Date.now()});
 		rooms[room].timestamp = videoTime + emitionDelay;
