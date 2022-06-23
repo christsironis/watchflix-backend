3b40c765ca0fc14b2a77ce64dbcd916739790f4e
chris <christsironiss@gmail.com>
2022-06-23 23:58:11 +0300

    remove emitiondelay from play and pause bcs we want to go back the duration of the video

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ io.on("connection", (socket) => {
 	socket.on("pause", ({ videoTime, user, room, dateEmited }) =>{
 		const dateNow = Date.now();
 		const emitionDelay = dateNow - dateEmited;
-		rooms[room].timestamp = (dateNow - rooms[room].date) ;
+		rooms[room].timestamp = (dateNow - rooms[room].date) - emitionDelay;
 		rooms[room].ispaused = true;
 		socket.to(room).emit("pause", {videoTime: rooms[room].timestamp, user: user, dateEmited: Date.now()});
 		console.log("user paused "," serverTimestamp= ", rooms[room].timestamp ," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
@@ -83,7 +83,7 @@ io.on("connection", (socket) => {
 		console.log("user played "," serverTimestamp= ", rooms[room].timestamp ," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
 
 		socket.to(room).emit("play", {videoTime: videoTime + emitionDelay, user: user, dateEmited: Date.now()});
-		rooms[room].timestamp = videoTime + emitionDelay;
+		rooms[room].timestamp = videoTime - emitionDelay;
 		rooms[room].ispaused = false;
 	});
 	socket.on("leave_room", ({ room, user}) => {
