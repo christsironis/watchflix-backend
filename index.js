253ef29482a96f0ec42cf1b45f3c23f088a9e366
chris <christsiroiss@gmail.com>
2022-06-24 16:22:26 +0300

    play video on all clients with the same emit

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ io.on("connection", (socket) => {
 		console.log("user paused "," serverTimestamp= ", rooms[room].timestamp ," videoTime= ",videoTime," emitionDelay= ",emitionDelay)
 	});
 	socket.on("play", ({ videoTime, user, room, dateEmited }) =>{
-		const dateNow = Date.now();
+		// const dateNow = Date.now();
 		// const emitionDelay = dateNow - dateEmited;
 		rooms[room].timestamp = videoTime;
 		rooms[room].date = dateNow -  rooms[room].timestamp;		
